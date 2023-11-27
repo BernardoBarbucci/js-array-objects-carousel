b041f91f92517fa1bee364b0f21f0a681919abef
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-27 17:26:59 +0100

    aggiunti buttons per scorrere e divisa in due div ulteriori il div per il carousel

--- script.js
+++ script.js
@@ -18,3 +18,5 @@
 // Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 // BONUS 3:
 // Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+
+// step .1 creo un array dove metto gli objects, ossia img + h1 + p
