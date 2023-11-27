ceddddb2172ee32411c54a0e4ed94a62546b2538
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-27 19:13:58 +0100

    onclick migliorato su js e rimosso da html

--- script.js
+++ script.js
@@ -24,23 +24,23 @@ const images = [
     {
         image: './img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morales',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        description: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: './img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        description: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
         image: './img/03.webp',
         title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        description: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
         image: 'img/04.webp',
         title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        description: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        description: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
 
@@ -55,10 +55,12 @@ function showSlide(index) {
      imgEl.src = slide.imgSrc;
      textEL.innerhtml = `
         <h1>${slide.title}</h1>
-        <h2>${slide.subtitle}</h2>
         <p>${slide.description}</p>
      `;
 }
+
+document.getElementById('prev-img').addEventListener('click', prevSlide);
+document.getElementById('next-img').addEventListener('click', nextSlide);
 
 function nextSlide() {
     currentIndex = (currentIndex + 1) % images.length;
